e2f8a0cd3995622bccbf7fa7a2c002f68fe86379
Evgeny Boganov <boganov@gmail.com>
2021-05-04 19:18:16 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -7,17 +7,9 @@ const staticAssets = [
 ];
 
 self.addEventListener('install', async event => {
-  const cache = await caches.open(cacheName);
-  await cache.addAll(staticAssets);
+  console.log('install');
 });
 
 self.addEventListener('fetch', event => {
-  const req = event.request;
-  event.respondWith(cacheFirst(req));
+  console.log('fetch);
 });
-
-async function cacheFirst(req) {
-  const cache = await caches.open(cacheName);
-  const cachedResponse = await cache.match(req);
-  return cachedResponse || fetch(req);
-}
